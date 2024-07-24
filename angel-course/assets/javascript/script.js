fc0b3b2eeda74d675390c63ea1488393968e42eb
Firsty Angelica Valency <149746443+angelvalency@users.noreply.github.com>
2024-07-24 20:22:45 +0700

    Delete angel-course directory

--- angel-course/assets/javascript/script.js
+++
@@ -1,9 +0,0 @@
-const qna = document.querySelectorAll(".qna")
-
-//click dropdown
-qnaElements.forEach (qna=> {
-    element.addEventListener("click", () =>
-    {
-        element.classList.toggle("active");
-    });
-});
