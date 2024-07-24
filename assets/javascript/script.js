a596b6e837bf781ad95c1440bcdd6663b1336055
Firsty Angelica Valency <149746443+angelvalency@users.noreply.github.com>
2024-07-24 20:23:28 +0700

    first add

---
+++ assets/javascript/script.js
@@ -0,0 +1,9 @@
+const qna = document.querySelectorAll(".qna")
+
+//click dropdown
+qnaElements.forEach (qna=> {
+    element.addEventListener("click", () =>
+    {
+        element.classList.toggle("active");
+    });
+});
